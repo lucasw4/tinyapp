30c25d9ddd8cca1a4594b8533e31ef2fd201acfa
Lucas Sahota <lucaswade4@gmail.com>
2022-10-27 20:38:30 -0700

    Updated expressserver.js to store short-urls in database and added redirects

--- express_server.js
+++ express_server.js
@@ -13,42 +13,37 @@ const urlDatabase = {
 const generateRandomString = function() {
   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const array = [];
-  while (array.length <= 6) {
+  while (array.length < 6) {
     array.push(alphabet[Math.ceil(Math.random() * 52)])
   }
   return array.join('')
 }
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post('/urls', (req, res) => {
-  console.log(req.body)
-  res.send("OK")
-})
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
-  res.render("urls_show", templateVars);
-});
 
 app.get('/urls', (req, res )=> {
   const templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars)
 })
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
 });
+
+app.post('/urls', (req, res) => {
+  let string = generateRandomString()
+  urlDatabase[string] = req.body.longURL
+  res.redirect(`/urls/${string}`)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
