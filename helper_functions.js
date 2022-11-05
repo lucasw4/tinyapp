6cf27ef3ef2508e2784b862345cc9a5d335cd4f9
Lucas Sahota <lucaswade4@gmail.com>
2022-11-05 16:04:33 -0700

    installing mocha and chai and slight refactoring of helperfunctions.js

--- helper_functions.js
+++ helper_functions.js
@@ -1,38 +1,38 @@
-const generateRandomString = function() {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// Takes 6 random letters from the alphabet to create a random string
+const generateRandomString = function () {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const array = [];
   while (array.length < 6) {
-    array.push(alphabet[Math.ceil(Math.random() * 52)])
+    array.push(alphabet[Math.ceil(Math.random() * 52)]);
   }
-  return array.join('')
-}
+  return array.join("");
+};
 
 // Helper function that finds either the email, userID or password using the third paramater, "e": email, "p": password, "i": userID
-const findUserByEmail = function(object, email, variable) {
+const findUserByEmail = function (object, email, variable) {
   for (let keys in object) {
     if (email === object[keys].email && variable === "e") {
       return email;
-    } else if (email === object[keys].email && variable === "p") {
-      return object[keys].password
-    } else if (email === object[keys].email && variable === "i") {
-      return object[keys].id
+    }
+    if (email === object[keys].email && variable === "p") {
+      return object[keys].password;
+    }
+    if (email === object[keys].email && variable === "i") {
+      return object[keys].id;
     }
   }
-  return null
-}
+  return null;
+};
 
+// Browses urlDatabase and finds all urls that match the userID provided
 const findUserUrls = function (userID, urlDatabase) {
   const urls = {};
   for (let url in urlDatabase) {
     if (userID === urlDatabase[url].userID) {
-      urls[url] = urlDatabase[url]
+      urls[url] = urlDatabase[url];
     }
   }
-  return urls
-}
-
-const urlDatabase = {
-  "b2xVn2": {longURL: "http://www.lighthouselabs.ca", id: "iVyfkz"}
+  return urls;
 };
 
-module.exports = { generateRandomString, findUserByEmail, findUserUrls }
+module.exports = { generateRandomString, findUserByEmail, findUserUrls };
