f0440f0aaca95ce38b89cb45e3305070450d128c
Lucas Sahota <lucaswade4@gmail.com>
2022-11-05 16:22:57 -0700

    Added tests for helper functions

---
+++ test/helperTests.js
@@ -0,0 +1,71 @@
+const { assert } = require("chai");
+
+const {
+  findUserByEmail,
+  generateRandomString,
+  findUserUrls,
+} = require("../helper_functions");
+
+const testUsers = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+const testDatabase = {
+  b2xVn2: { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+};
+
+describe("findUserByEmail", function () {
+  it("should return a user with valid email", function () {
+    const user = findUserByEmail(testUsers, "user@example.com", "e");
+    const expectedEmail = "user@example.com";
+    // Write your assert statement here
+    assert.equal(expectedEmail, user);
+  });
+  it("should return a user's pass with valid email", function () {
+    const user = findUserByEmail(testUsers, "user2@example.com", "p");
+    const expectedPass = "dishwasher-funk";
+
+    assert.equal(expectedPass, user);
+  });
+  it("should return a user's userID with valid email", function () {
+    const user = findUserByEmail(testUsers, "user2@example.com", "i");
+    const expectedID = "user2RandomID";
+
+    assert.equal(expectedID, user);
+  });
+  it("should return null with invalid email", function () {
+    const user = findUserByEmail(testUsers, "user243@example.com", "p");
+    const expectedResult = null;
+
+    assert.equal(expectedResult, user);
+  });
+});
+
+describe("generateRandomString", function () {
+  it("should generate a random 6 letter string", function () {
+    const result = generateRandomString();
+    const expectedLength = 6;
+    assert.equal(result.length, expectedLength);
+  });
+  it("should generate a random string, this test has a 1/52 chance of failing", function () {
+    const result = generateRandomString();
+    assert.isFalse(result[0] === "P");
+  });
+});
+
+describe("findUserUrls", function () {
+  it("should find all the urls in a database that belong to a userID", function () {
+    const result = findUserUrls("userRandomID", testDatabase);
+    const expectedResult = testDatabase["b2xVn2"].longURL;
+    assert.equal(result["b2xVn2"].longURL, expectedResult);
+  });
+});
